4dc907779e6058de909b16a561e6f06f7fc581cd
Buntin-Synthia <aokitakumi72@gmail.com>
2023-04-23 21:25:39 +0900

    mukatukus space

--- src/index.ts
+++ src/index.ts
@@ -63,8 +63,6 @@ const commands: Command[] = [
 const app = client.application;
 app?.commands.set(commands.map((command) => command.data));
 
-
-
 client.once(Events.ClientReady, (c: Client) => {
     console.log(`Ready! Logged in as ${c.user?.tag}`);
 });
